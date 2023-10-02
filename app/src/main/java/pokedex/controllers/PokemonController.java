34c662de789010e2033fccd90bdc905dfd3f6bb3
Swendart <sophie@curinier.fr>
2023-10-02 12:20:37 -0400

    Update pokemon controller

--- app/src/main/java/pokedex/controllers/PokemonController.java
+++ app/src/main/java/pokedex/controllers/PokemonController.java
@@ -1,13 +1,43 @@
 package pokedex.controllers;
+import pokedex.models.PokemonModel;
+import pokedex.views.PokemonView;
 
 public class PokemonController {
     // Attribute
+    private PokemonModel model;
+    private PokemonView view;
 
     // Constructor
     public PokemonController() {
+        model = new PokemonModel();
+        view = new PokemonView();
+    }
+
+    public PokemonController(PokemonModel model, PokemonView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    // Setters
+    public void setPokemonModel(int id, String name, int height, int weight, String description) {
+        model.setId(id);
+        model.setName(name);
+        model.setHeight(height);
+        model.setWeight(weight);
+        model.setDescription(description);
     }
 
     // Getters
+    public PokemonModel getModel() {
+        return model;
+    }
 
-    // Setters
+    public PokemonView getView() {
+        return view;
+    }
+
+    // Methods
+    public void updateView() {
+        view.setView(model.getId(), model.getName(), model.getHeight(), model.getWeight(), model.getDescription());
+    }
 }
--- app/src/test/java/pokedex/controllers/PokemonControllerTest.java
+++ app/src/test/java/pokedex/controllers/PokemonControllerTest.java
@@ -1,4 +1,6 @@
 package pokedex.controllers;
+import pokedex.models.PokemonModel;
+import pokedex.views.PokemonView;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,4 +15,20 @@ public class PokemonControllerTest {
     @BeforeEach public void setUp() {
         controller = new PokemonController();
     }
+
+    @Test public void testSetPokemonModel() {
+        controller.setPokemonModel(1, "Bulbizzare", 7, 69, "Love water.");
+        assertEquals(1, controller.getModel().getId());
+        assertEquals("Bulbizzare", controller.getModel().getName());
+        assertEquals(7, controller.getModel().getHeight());
+        assertEquals(69, controller.getModel().getWeight());
+        assertEquals("Love water.", controller.getModel().getDescription());
+    }
+
+    @Test public void testUpdateView() {
+        controller.setPokemonModel(1, "Bulbizzare", 7, 2, "Love water.");
+        controller.updateView();
+        assertEquals("=============================\nPokémon #1\nName : Bulbizzare\nHeight : 7\nWeight : 2\nDescription : Love water.\n=============================\n",controller.getView().getView());
+    }
+    
 }
